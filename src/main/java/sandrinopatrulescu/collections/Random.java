2f060710207f5bb0e071ec018abc61333cda5e49
sandrinopatrulescu <a>
2022-10-03 09:46:25 +0300

    +method to generate random string +methods to choose randomly from array and from enum values

--- src/main/java/sandrinopatrulescu/collections/Random.java
+++ src/main/java/sandrinopatrulescu/collections/Random.java
@@ -1,6 +1,7 @@
 package sandrinopatrulescu.collections;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 public class Random {
     // java generate random integer in range https://stackoverflow.com/a/363692/17299754
@@ -14,5 +15,33 @@ public class Random {
     public static int nextInt(int minimum, int maximum) {
         // https://stackoverflow.com/a/27156435/17299754
         return r.ints(minimum, maximum + 1).findFirst().getAsInt();
+    }
+
+    public static String randomString(int length, char[] chars) {
+        return r.ints(0, chars.length)
+                .limit(length)
+                .collect(StringBuilder::new, (sb, i) -> sb.append(chars[i]), StringBuilder::append)
+                .toString();
+    }
+
+    public static String randomStringOfASCIIPrintableChars(int length) {
+        int firstASCIIPrintableCharacter = 32;
+        int lastASCIIPrintableCharacter = 126;
+
+        return randomString(
+                length,
+                IntStream.range(firstASCIIPrintableCharacter, lastASCIIPrintableCharacter + 1)
+                        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                        .toString().
+                        toCharArray()
+        );
+    }
+
+    public static <T> T chooseFromArray(T[] array) {
+        return array[r.nextInt(array.length)];
+    }
+
+    public static <T extends Enum<T>> T chooseFromEnum(T[] array) {
+        return array[r.nextInt(array.length)];
     }
 }
